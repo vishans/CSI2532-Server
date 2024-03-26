7c76aff513d99452089a24e27724d8d2206940c0
Vishan Sonagadu <vishansonagadu@gmail.com>
2024-03-26 17:03:20 -0400

    Add POST hotel/newClient as a replacement to POST hotel/newclient. The later still works; backward compatible.

--- routes/hotelRoutes.js
+++ routes/hotelRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const hotelController  = require('../controllers/hotelController');
 const router = express.Router();
 
-router.route('/newclient').post(hotelController.createClient)
+router.route('/newclient').post(hotelController.createClient) // deprecated but still works for legacy reasons.
+router.route('/createClient').post(hotelController.createClient)
+
 router.route('/verify').get(hotelController.verify)
 
  
